46df723346bd6653ad0aea05c4175ec89b2399ee
jeffa <jeffa@unlocalhost.com>
2017-02-04 18:11:59 -0800

    more progress but more questions ...

--- lib/Test/Generate/Lang/Perl.pm
+++ lib/Test/Generate/Lang/Perl.pm
@@ -6,8 +6,60 @@ use Text::Template;
 
 sub _generate {
     my $data = shift;
-    my $tmpl = Text::Template->new( TYPE => 'FILEHANDLE', SOURCE => *DATA );
+    my $tmpl = Text::Template->new( TYPE => 'STRING', SOURCE => _exec_template() );
+    my $perl = $tmpl->fill_in( HASH => $data );
+    #my $results = eval $perl;
+    $tmpl = Text::Template->new( TYPE => 'STRING', SOURCE => _test_template() );
     return $tmpl->fill_in( HASH => $data );
+}
+
+sub _exec_template {
+return q^
+use {$class{name}};
+my ${$class{instance}} = {$class{name}}->new( { join( ', ', map join(' => ', @$_), @{$class{args}} ) } );
+{ for (@tests) {
+    $OUT .= sprintf( qq{print(%s,\$/);\n},
+        sprintf( $_->{filter} || '%s',
+            sprintf( '$%s->%s( %s )', 
+                $class{instance},
+                $_->{method},
+                join( ', ', @{$_->{args}} ),
+            )
+        ),
+        $_->{result},
+        $_->{name},
+    )
+  }
+}
+^;
+}
+
+sub _test_template {
+return q/
+#!perl -T
+use 5.006;
+use strict;
+use warnings;
+use Test::More tests => {@tests + 1};
+use {$class{name}};
+
+my ${$class{instance}} = new_ok '{$class{name}}', [ { join( ', ', map join(' => ', @$_), @{$class{args}} ) } ];
+
+{ for (@tests) {
+    $OUT .= sprintf( qq(is %s, "%s", "%s";\n),
+        sprintf( $_->{filter} || '%s',
+            sprintf( '$%s->%s( %s )', 
+                $class{instance},
+                $_->{method},
+                join( ', ', @{$_->{args}} ),
+            )
+        ),
+        $_->{result},
+        $_->{name},
+    )
+  }
+}
+/;
 }
 
 1;
@@ -65,47 +117,3 @@ CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
-
-__DATA__
-#!perl -T
-use 5.006;
-use strict;
-use warnings;
-use Test::More tests => {$tests};
-
-{ for (@include) { $OUT .= "use $_;\n" } }
-
-my $mapper = new_ok 'Math::Window2Viewport', [
-    Wb => 0, Wt => 1, Wl => 0, Wr => 1,
-    Vb => 9, Vt => 0, Vl => 0, Vr => 9,
-];
-
-is int( $mapper->Dx( .5 ) ), '4',   "correct Dx()";
-is int( $mapper->Dy( .6 ) ), '3',   "correct Dx()";
-
-my ($x, $y);
-
-$x = 0;
-$y = sin( $x );
-is int( $mapper->Dx( $x ) ), 0,   "correct Dx() sin wave 1/5";
-is int( $mapper->Dy( $y ) ), 9,   "correct Dy() sin wave 1/5";
-
-$x = 0.1;
-$y = sin( $x );
-is int( $mapper->Dx( $x ) ), 0,   "correct Dx() sin wave 2/5";
-is int( $mapper->Dy( $y ) ), 8,   "correct Dy() sin wave 2/5";
-
-$x = 0.2;
-$y = sin( $x );
-is int( $mapper->Dx( $x ) ), 1,   "correct Dx() sin wave 3/5";
-is int( $mapper->Dy( $y ) ), 7,   "correct Dy() sin wave 3/5";
-
-$x = 0.3;
-$y = sin( $x );
-is int( $mapper->Dx( $x ) ), 2,   "correct Dx() sin wave 4/5";
-is int( $mapper->Dy( $y ) ), 6,   "correct Dy() sin wave 4/5";
-
-$x = 0.4;
-$y = sin( $x );
-is int( $mapper->Dx( $x ) ), 3,   "correct Dx() sin wave 5/5";
-is int( $mapper->Dy( $y ) ), 5,   "correct Dy() sin wave 5/5";
